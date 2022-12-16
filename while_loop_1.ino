464f04f01aed865fc8bd218bd686492916c50b55
INJINDAISHOGUN <118966450+INJINDAISHOGUN@users.noreply.github.com>
2022-12-16 18:32:31 +0700

    first commit

---
+++ while_loop_1.ino
@@ -0,0 +1,19 @@
+int led1 = 12;
+int led2 = 15;
+void setup() {
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
+  digitalWrite(led1,HIGH);
+
+}
+
+void loop() {
+  int x=10;
+  while(x >= 1){
+    digitalWrite(led1,LOW);
+    delay(1000);
+    digitalWrite(led1,HIGH);
+    delay(1000);
+    x--;
+  }
+}
